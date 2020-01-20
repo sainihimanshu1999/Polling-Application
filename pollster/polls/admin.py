d71f00035e589f3ab17816dc38fd570ee4b0ec79
Himanshu Saini <sainihimanshu.1999@gmail.com>
2020-01-20 23:13:23 +0530

    Initial Commit

---
+++ pollster/polls/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+
+from.models import Question, Choice
+
+admin.site.site_header = 'Pollster Admin'
+admin.site.site_title = 'Pollster admin area'
+admin.site.index_title = 'Welcome to Pollster admin area'
+
+class ChoiceInline(admin.TabularInline):
+    model= Choice
+    extra = 3 
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None , {'fields': ['question_text']}),
+                 ('Date Information' , {'fields': ['pub_date'],'classes' :
+                 ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
+#admin.site.register(Question)
+#admin.site.register(Choice)
+
